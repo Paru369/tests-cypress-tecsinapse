ebdf7c06ee0753e27f5b29b0858e0ac1baf1ed5b
Paru - Paulo Victor Pinheiro <paruvitu@gmail.com>
2022-06-28 17:09:49 -0300

    clena

--- cypress/e2e/tecsinapse-test-web/tecsinapse.cy.js
+++ cypress/e2e/tecsinapse-test-web/tecsinapse.cy.js
@@ -84,52 +84,6 @@ describe('Web TecSinapse', ()=> {
 
 
 
-  // it('verifica msg de senha pequena', ()=>{
-
-  //   cy.contains('a','Register now').click();
-  //   cy.contains('button','Register').click();
-  //   cy.get('input[formcontrolname="password"]').type("p")
-  //   cy.contains('button','Register').click();
-  //   cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
-
-  // } )
-
-  // it('verifica user pequeno', ()=>{
-
-  //   cy.contains('a','Register now').click();
-  //   cy.contains('button','Register').click();
-  //   cy.get('input[formcontrolname="fullName"]').type("p")
-  //   cy.contains('button','Register').click();
-  //   cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
-
-  // } )
-
-  // it('verify user lowcase', ()=>{
-
-  //   cy.contains('a','Register now').click();
-  //   cy.contains('button','Register').click();
-  //   cy.get('input[formcontrolname="userName"]').type("PPP")
-  //   cy.contains('button','Register').click();
-  //   cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
-
-  // } )
-
-  // it('fazer login de usuario valido', () =>{
-  //   cy.login('flavio', '123');
-  //   cy.contains('a', '(Logout)').should('be.visible');
-  // })
-
-  // it('fazer login de usuario invalido', () =>{
-  //   cy.login('jaqueline', '1234');
-  //   cy.on('window:alert', (str) => {
-  //     expect(str).to.equal('Invalid user name or password')
-  //   })
-
-  // });
-
-  // const usuarios = require('../../fixtures/usuarios.json');
-  // usuarios.forEach(usuario =>{})
-    
 
   } );
 
